ff73b1e995f77f3a815d6dc463b90dd64483f823
Abhishek Prasad <devcula@outlook.com>
2020-03-23 01:46:08 +0530

    Updates

---
+++ precache-manifest.69a36f5b663a55a794346ee07a718894.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc78dbdcb54be6c6de1779940556859",
+    "url": "/corona-monitor/index.html"
+  },
+  {
+    "revision": "fbd6d260bd5fd0c57aad",
+    "url": "/corona-monitor/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "56675d4f2a69059992ec",
+    "url": "/corona-monitor/static/css/main.0fb35ec0.chunk.css"
+  },
+  {
+    "revision": "fbd6d260bd5fd0c57aad",
+    "url": "/corona-monitor/static/js/2.502e7f6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corona-monitor/static/js/2.502e7f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56675d4f2a69059992ec",
+    "url": "/corona-monitor/static/js/main.76799863.chunk.js"
+  },
+  {
+    "revision": "2b67d12029d853cdc3a1",
+    "url": "/corona-monitor/static/js/runtime-main.ab5c5644.js"
+  }
+]);
