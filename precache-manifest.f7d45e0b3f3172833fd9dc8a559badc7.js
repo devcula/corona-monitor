ff7e2ee55a56221e86de6c3fdcbdda166602e503
Abhishek Prasad <devcula@outlook.com>
2020-04-02 01:24:06 +0530

    Updates

---
+++ precache-manifest.f7d45e0b3f3172833fd9dc8a559badc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc73631b9744cbcb4c1def3ea948c45",
+    "url": "/corona-monitor/index.html"
+  },
+  {
+    "revision": "19fd22bacf389364c41b",
+    "url": "/corona-monitor/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d12c97b7957e8455b856",
+    "url": "/corona-monitor/static/css/main.2ddf2794.chunk.css"
+  },
+  {
+    "revision": "19fd22bacf389364c41b",
+    "url": "/corona-monitor/static/js/2.d2f14536.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corona-monitor/static/js/2.d2f14536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12c97b7957e8455b856",
+    "url": "/corona-monitor/static/js/main.3612b75a.chunk.js"
+  },
+  {
+    "revision": "2b67d12029d853cdc3a1",
+    "url": "/corona-monitor/static/js/runtime-main.ab5c5644.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corona-monitor/precache-manifest.2a1463f37595bca29d16e3b72e1432d9.js"
+  "/corona-monitor/precache-manifest.f7d45e0b3f3172833fd9dc8a559badc7.js"
 );
 
 self.addEventListener('message', (event) => {
