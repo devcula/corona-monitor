2cadf546f91fc946aa8eaeafc99275fbd69f4316
Abhishek Prasad <devcula@outlook.com>
2020-04-02 01:18:27 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corona-monitor/precache-manifest.51cd4d259e35e2cf4c9586fa563ca494.js"
+  "/corona-monitor/precache-manifest.2a1463f37595bca29d16e3b72e1432d9.js"
 );
 
 self.addEventListener('message', (event) => {
