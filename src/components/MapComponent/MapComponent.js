46253622db9a386a2662fc89d51954709bf56f19
Abhishek Prasad <devcula@outlook.com>
2020-04-08 20:40:32 +0530

    Code cleanup

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -1,11 +1,10 @@
 import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
-import useSWR from "swr"; // React hook to fetch the data
-import lookup from "country-code-lookup"; // npm module to get ISO Code for countries
+import useSWR from "swr";
+import lookup from "country-code-lookup";
 
 import "./Map.scss";
 
-// Mapbox css - needed to make tooltips work later in this article
 import "mapbox-gl/dist/mapbox-gl.css";
 
 mapboxgl.accessToken = "pk.eyJ1IjoiZGV2Y3VsYSIsImEiOiJjazhxemlxcDQwMmd5M2tsb3pjeWhmdTVtIn0._8vkTS-En-GaheV1Sy2djQ";
@@ -26,7 +25,7 @@ function MapComponent() {
                         ]
                     },
                     properties: {
-                        id: index, // unique identifier in this case the index
+                        id: index,
                         country: point.country,
                         province: point.province,
                         cases: point.stats.confirmed,
@@ -35,28 +34,21 @@ function MapComponent() {
                 }))
             );
 
-    // Fetching our data with swr package
     const { data } = useSWR("https://corona.lmao.ninja/v2/jhucsse", fetcher);
-    const mapboxElRef = useRef(null); // DOM element to render map
+    const mapboxElRef = useRef(null);
 
-    // Initialize our map
     useEffect(() => {
         console.log(data);
-        // You can store the map instance with useRef too
         if (data) {
             const map = new mapboxgl.Map({
                 container: mapboxElRef.current,
                 style: "mapbox://styles/mapbox/dark-v10",
-                center: [16, 27], // initial geo location
-                zoom: 2 // initial zoom
+                center: [16, 27],
+                zoom: 2
             });
-            // Add navigation controls to the top right of the canvas
             map.addControl(new mapboxgl.NavigationControl());
 
-            // Call this method when the map is loaded
             map.once("load", function () {
-                // Add our SOURCE
-                // with id "points"
                 map.addSource("points", {
                     type: "geojson",
                     data: {
@@ -65,12 +57,10 @@ function MapComponent() {
                     }
                 });
 
-                // Add our layer
                 map.addLayer({
                     id: "circles",
-                    source: "points", // this should be the id of the source
+                    source: "points",
                     type: "circle",
-                    // paint properties
                     paint: {
                         "circle-opacity": 0.75,
                         "circle-stroke-width": [
@@ -105,31 +95,23 @@ function MapComponent() {
                         ],
                     }
                 });
-                // Create a mapbox popup
                 const popup = new mapboxgl.Popup({
                     closeButton: false,
                     closeOnClick: false
                 });
 
-                // Variable to hold the active country/province on hover
                 let lastId;
-
-                // Mouse move event
                 map.on("mousemove", "circles", e => {
-                    // Get the id from the properties
                     const id = e.features[0].properties.id;
 
-                    // Only if the id are different we process the tooltip
                     if (id !== lastId) {
                         lastId = id;
 
-                        // Change the pointer type on move move
                         map.getCanvas().style.cursor = "pointer";
 
                         const { cases, deaths, country, province } = e.features[0].properties;
                         const coordinates = e.features[0].geometry.coordinates.slice();
 
-                        // Get all data for the tooltip
                         const countryISO =
                             lookup.byCountry(country)?.iso2 || lookup.byInternet(country)?.iso2;
 
@@ -153,9 +135,6 @@ function MapComponent() {
                         <div>
                         `;
 
-                        // Ensure that if the map is zoomed out such that multiple
-                        // copies of the feature are visible, the popup appears
-                        // over the copy being pointed to.
                         while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
                             coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
                         }
@@ -167,9 +146,7 @@ function MapComponent() {
                     }
                 });
 
-                // Mouse leave event
                 map.on("mouseleave", "circles", function () {
-                    // Reset the last Id
                     lastId = undefined;
                     map.getCanvas().style.cursor = "";
                     popup.remove();
@@ -181,7 +158,6 @@ function MapComponent() {
     return (
         <div className="MapComponent component-div">
             <div className="mapContainer">
-                {/* Assigned Mapbox container */}
                 <div className="mapBox" ref={mapboxElRef} />
             </div>
         </div>
